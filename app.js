16835c003ee30912aa5426eb77f1e8815822a8a0
eyupGuc <meydanguresi@gmail.com>
2022-09-19 20:39:12 +0300

    set input

---
+++ app.js
@@ -0,0 +1,9 @@
+const addBtn = document.querySelector("#todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
+
+addBtn.addEventListener("click", () => {
+  if (todoInput.value.trim() === "") {
+    alert("Please enter new todo");
+  }
+});
