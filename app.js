e052ce5e55161dfc0e3527650a81f460b4825b2b
eyupGuc <meydanguresi@gmail.com>
2022-09-19 20:58:12 +0300

    united

--- app.js
+++ app.js
@@ -5,5 +5,23 @@ const todoUl = document.getElementById("todo-ul");
 addBtn.addEventListener("click", () => {
   if (todoInput.value.trim() === "") {
     alert("Please enter new todo");
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
   }
+  createListElement(newTodo);
 });
+
+const createListElement = (newTodo) => {
+  const li = document.createElement("li");
+
+  // li.id=newTodo.id
+  li.setAttribute("id", newTodo.id);
+  console.log(li);
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+};
